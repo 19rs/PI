7698ab74dee728ef85b7cff438446d45456dbb9f
rw09 <rodrigosouza170487@gmail.com>
2024-06-19 10:28:01 -0300

    Mensagens no db

--- src/dist/app.js
+++ src/dist/app.js
@@ -6,7 +6,7 @@ import fileUpload from "express-fileupload"
 import usuarioRouter from '../routes/usuarioRouter.js'
 // import mensagemRouter from '../routes/mensagemRouter.js'
 // import respostaRouter from '../routes/respostasRouter.js'
-import resposta_respostaRouter from "../routes/resposta_respostaRouter.js"
+// import resposta_respostaRouter from "../routes/resposta_respostaRouter.js"
 import forumRouter from '../routes/forumRouter.js'
 
 const app = express();
@@ -18,10 +18,9 @@ app.use(fileUpload());
 app.use(authRouter)
 app.use(pessoasRouter)
 app.use(usuarioRouter)
-// app.use(mensagemRouter)
 app.use(forumRouter)
 // app.use(respostaRouter)
-app.use(resposta_respostaRouter)
+// app.use(resposta_respostaRouter)
 
 
 
